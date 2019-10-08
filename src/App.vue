a16b52c8a338ba403dd825e05af9400bf6d72d67
jackie <jacqueline.kartchner@gmail.com>
2019-10-07 19:28:04 -0700

    adds CRUD routes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/signup">Signup</router-link>
+      |
+      <router-link to="/login">Login</router-link>
+      |
+      <router-link to="/logout">Logout</router-link>
+      |
+      <router-link to="/MoviesIndex">Movies Index</router-link>
+      |
+      <router-link to="/MoviesCreate">Movies Create</router-link>
+      |
+      <router-link to="/MoviesUpdate">Movies Update</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/router.js
+++ src/router.js
@@ -1,25 +1,45 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
+import MoviesIndex from "./views/movies/MoviesIndex.vue";
+import MoviesCreate from "./views/movies/MoviesCreate.vue";
+import MoviesUpdate from "./views/movies/MoviesUpdate.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/MoviesIndex",
+      name: "movies-index",
+      component: MoviesIndex
+    },
+    {
+      path: "/MoviesCreate",
+      name: "movies-create",
+      component: MoviesCreate
+    },
+    { path: "/MoviesUpdate", name: "movies-update", component: MoviesUpdate },
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout }
   ]
-})
+});
